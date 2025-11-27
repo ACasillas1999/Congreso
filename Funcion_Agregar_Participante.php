c39a316a0295988553c21aae14af7b1344abefba
ACasillas1999 <acasillas.work@gmail.com>
2025-11-26 18:18:52 -0600

    Update Funcion_Agregar_Participante.php

--- Funcion_Agregar_Participante.php
+++ Funcion_Agregar_Participante.php
@@ -1,8 +1,8 @@
 <?php
 
-//ini_set('display_errors', 1); 
-//ini_set('display_startup_errors', 1);
-//error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
  //Incluir la biblioteca QR Code
 require_once 'phpqrcode/qrlib.php';
 
