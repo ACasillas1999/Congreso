f2b6e58fa124ae75fa786933b0425c78f1de443c
ACasillas1999 <acasillas.work@gmail.com>
2026-03-19 17:53:59 -0600

    Revert "Update config.php"
    This reverts commit d7fba71652dd3c0f080c42ac6f97f3189098273b.

--- Conexiones/config.php
+++ Conexiones/config.php
@@ -1,6 +1,6 @@
 <?php
 if (!defined('DB_SERVER')) {
-    define('DB_SERVER', '18.211.75.118');
+    define('DB_SERVER', 'localhost');
 }
 
 if (!defined('DB_USERNAME')) {
@@ -8,7 +8,7 @@ if (!defined('DB_USERNAME')) {
 }
 
 if (!defined('DB_PASSWORD')) {
-    define('DB_PASSWORD', '04nm2fdLefCxM');
+    define('DB_PASSWORD', '');
 }
 
 if (!defined('DB_NAME')) {
