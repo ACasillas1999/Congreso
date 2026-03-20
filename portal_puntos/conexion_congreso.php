06fa710ef7d1bc5c40010c843fb582789f4c46f4
ACasillas1999 <acasillas.work@gmail.com>
2026-03-19 18:53:56 -0600

    Conexion

--- portal_puntos/conexion_congreso.php
+++ portal_puntos/conexion_congreso.php
@@ -1,14 +1,13 @@
 <?php
-define('DB_SERVER', 'localhost'); 
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'gpoascen_congresos');
-// Conectar a la base de datos
-$conn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+$dbServer = 'localhost';
+$dbUsername = 'root';
+$dbPassword = '';
+$dbName = 'gpoascen_congresos';
+
+$conn = new mysqli($dbServer, $dbUsername, $dbPassword, $dbName);
 $conn->set_charset("utf8mb4");
 
-// Verificar la conexión
 if ($conn->connect_error) {
-    die("Error de conexión: " . $conn->connect_error);
+    die("Error de conexion: " . $conn->connect_error);
 }
 ?>
